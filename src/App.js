8ec6fddc18e6fea7a7acbc4efe10c26ec6b719a9
Jigar Gosar <jigar.gosar@gmail.com>
2019-03-13 19:17:39 +0530

    zipper

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
 import React from 'react'
-import { nodeById, useAppModel } from './main'
+import { useAppModel } from './main'
 import validate from 'aproba'
 import * as Zipper from './tree-zipper'
 import * as Tree from './tree'
 import * as R from 'ramda'
 
-function RootTree({ model, effects }) {
-  function renderTitle(node) {
-    validate('O', arguments)
-    return (
-      <div
-        className="pa2"
-        tabIndex={0}
-        onClick={() => effects.newLine(node.id)}
-      >
-        {node.title}
-      </div>
-    )
-  }
-
-  function renderNodeTree(level, id) {
-    validate('NS', arguments)
-    const node = nodeById(id, model)
-    return [
-      <div key={id} style={{ paddingLeft: `${level * 1.5}rem` }}>
-        {renderTitle(node)}
-      </div>,
-      ...node.childIds.map(cid => renderNodeTree(level + 1, cid)),
-    ]
-  }
-
-  return renderNodeTree(0, model.rootId)
-}
+// function RootTree({ model, effects }) {
+//   function renderTitle(node) {
+//     validate('O', arguments)
+//     return (
+//       <div
+//         className="pa2"
+//         tabIndex={0}
+//         onClick={() => effects.newLine(node.id)}
+//       >
+//         {node.title}
+//       </div>
+//     )
+//   }
+//
+//   function renderNodeTree(level, id) {
+//     validate('NS', arguments)
+//     const node = nodeById(id, model)
+//     return [
+//       <div key={id} style={{ paddingLeft: `${level * 1.5}rem` }}>
+//         {renderTitle(node)}
+//       </div>,
+//       ...node.childIds.map(cid => renderNodeTree(level + 1, cid)),
+//     ]
+//   }
+//
+//   return renderNodeTree(0, model.rootId)
+// }
 
 function RootZipper({ model, effects }) {
   function renderTitle(node) {
@@ -40,7 +40,7 @@ function RootZipper({ model, effects }) {
       <div
         className="pa2"
         tabIndex={0}
-        onClick={() => effects.newLineZ(node.id)}
+        // onClick={() => effects.newLineZ(node.id)}
       >
         {node.title}
       </div>
@@ -74,7 +74,7 @@ function App() {
 
   return (
     <div className="">
-      <RootTree model={model} effects={effects} />
+      {/*<RootTree model={model} effects={effects} />*/}
       <RootZipper model={model} effects={effects} />
     </div>
   )
