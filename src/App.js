bb857fa0d54d39ca01675d4d50307c950262401c
Jigar Gosar <jigar.gosar@gmail.com>
2019-03-13 16:07:29 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,51 +1,8 @@
-import React, { useEffect, useMemo, useState } from 'react'
-import * as R from 'ramda'
-import validate from 'aproba'
-import { getCached, setCache } from './cache-helpers'
-
-function useEffects(setState) {
-  return useMemo(
-    () => ({
-      log: msg => console.log(msg),
-      setRootLabel: () =>
-        setState(state =>
-          R.assocPath(['byId', state.rootId, 'title'])('Root1')(state),
-        ),
-    }),
-    [],
-  )
-}
-
-function rootNode(model) {
-  validate('O', arguments)
-  return model.byId[model.rootId]
-}
+import React from 'react'
+import { rootNode, useAppModel } from './main'
 
 function Node({ node }) {
   return <div className="">{node.title}</div>
-}
-
-function useAppModel() {
-  const [model, setModel] = useState(() => {
-    const rootId = 'n_root'
-    const root = { id: rootId, title: 'Root', childIds: [] }
-
-    const def = { byId: { [root.id]: root }, rootId }
-
-    return R.compose(
-      // R.tap(console.log),
-      R.mergeDeepRight(def),
-      R.defaultTo({}),
-      getCached,
-    )('app-model')
-  })
-
-  useEffect(() => {
-    setCache('app-model', model)
-  }, [model])
-
-  const effects = useEffects(setModel)
-  return [model, effects]
 }
 
 function App() {
---
+++ src/main.js
@@ -0,0 +1,45 @@
+import { useEffect, useMemo, useState } from 'react'
+import * as R from 'ramda'
+import { getCached, setCache } from './cache-helpers'
+import validate from 'aproba'
+
+function useEffects(setState) {
+  return useMemo(
+    () => ({
+      log: msg => console.log(msg),
+      setRootLabel: () =>
+        setState(state =>
+          R.assocPath(['byId', state.rootId, 'title'])('Root1')(state),
+        ),
+    }),
+    [],
+  )
+}
+
+export function rootNode(model) {
+  validate('O', arguments)
+  return model.byId[model.rootId]
+}
+
+export function useAppModel() {
+  const [model, setModel] = useState(() => {
+    const rootId = 'n_root'
+    const root = { id: rootId, title: 'Root', childIds: [] }
+
+    const def = { byId: { [root.id]: root }, rootId }
+
+    return R.compose(
+      // R.tap(console.log),
+      R.mergeDeepRight(def),
+      R.defaultTo({}),
+      getCached,
+    )('app-model')
+  })
+
+  useEffect(() => {
+    setCache('app-model', model)
+  }, [model])
+
+  const effects = useEffects(setModel)
+  return [model, effects]
+}
