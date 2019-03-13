91dee771bb3ce89f353737024539ea2a7d350d2d
Jigar Gosar <jigar.gosar@gmail.com>
2019-03-13 18:02:47 +0530

    zipper

--- src/tree-zipper.js
+++ src/tree-zipper.js
@@ -1,6 +1,21 @@
 import validate from 'aproba'
+import * as R from 'ramda'
 
 export function singleton(tree) {
   validate('O', arguments)
   return { left: [], center: tree, right: [], crumbs: [] }
+}
+
+export function appendChildGoR(child, z) {
+  validate('OO', arguments)
+  return {
+    left: z.center.children,
+    center: child,
+    right: [],
+    crumbs: R.prepend({
+      left: z.left,
+      datum: z.center.datum,
+      right: z.right,
+    })(z.crumbs),
+  }
 }
--- src/tree.js
+++ src/tree.js
@@ -1,6 +1,12 @@
 import validate from 'aproba'
+import * as R from 'ramda'
 
 export function fromDatum(datum) {
   validate('*', arguments)
   return { datum, children: [] }
+}
+
+export function appendChild(child, parent) {
+  validate('OO', arguments)
+  return R.over(R.lensPath(['children']), R.append(child))(parent)
 }
