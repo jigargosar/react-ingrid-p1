de1adc92bf13033f4c5e08ca9ad41ed575b1d61c
Jigar Gosar <jigar.gosar@gmail.com>
2019-03-13 15:41:52 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
+import * as R from 'ramda'
 
 function App() {
-  const [state, setState] = useState(() => ({
-    tree: { datum: 'Root', children: [] },
+  const [model, setState] = useState(() => ({
+    root: { title: 'Root', children: [] },
   }))
+
+  const effects = useMemo(
+    () => ({
+      log: msg => console.log(msg),
+      setRootLabel: () =>
+        setState(R.assocPath(['root', 'title'])('Root1')),
+    }),
+    [model],
+  )
 
   return (
     <div className="">
-      <div className="">{state.tree.datum}</div>
+      <div className="" onClick={effects.log}>
+        {model.root.title}
+      </div>
     </div>
   )
 }
