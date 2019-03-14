c56289912830aa6efded2ce55684ebd125b6f37a
Jigar Gosar <jigar.gosar@gmail.com>
2019-03-14 14:45:05 +0530

    zipper tests

--- src/LineZipper.test.js
+++ src/LineZipper.test.js
@@ -3,6 +3,7 @@ import * as LineZipper from './LineZipper'
 describe('LineZipper', function() {
   it('should have initial value', function() {
     const z = LineZipper.initial
+
     expect(z).toBeDefined()
   })
 })
