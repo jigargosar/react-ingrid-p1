cd8874fe4cb7c9b0386403b0dd079dc0ad8c9604
Jigar Gosar <jigar.gosar@gmail.com>
2019-03-14 15:33:34 +0530

    zipper tests

--- src/LineZipper.test.js
+++ src/LineZipper.test.js
@@ -2,28 +2,38 @@ import * as LineZipper from './LineZipper'
 import * as Zipper from './TreeZipper'
 import { pipe } from 'ramda'
 import * as LineTree from './LineTree'
+import * as Tree from './Tree'
+import validate from 'aproba'
+
+function anyParentCollapsed(z) {
+  validate('O', arguments)
+  const pz = Zipper.parent(z)
+  return pz ? Zipper.datum(pz).collapsed || anyParentCollapsed(pz) : false
+}
 
 describe('LineZipper', function() {
   it('should have initial value', function() {
     expect(LineZipper.initial).toBeDefined()
   })
   it('should do something', function() {
-    const z = pipe(Zipper.appendGoR(LineTree.newLine()))(
-      LineZipper.initial,
-    )
+    const z = pipe(
+      Zipper.appendChildGoR(
+        Tree.fromDatum({ collapsed: true, title: 'rc1' }),
+      ),
+      Zipper.appendChildGoR(
+        Tree.fromDatum({ collapsed: false, title: 'rc1-1' }),
+      ),
+      Zipper.parent,
+      Zipper.appendChildGoR(
+        Tree.fromDatum({ collapsed: true, title: 'rc2' }),
+      ),
+      Zipper.findPrev(z => {
+        return !anyParentCollapsed(z)
+      }),
+    )(Zipper.singleton(Tree.fromDatum({ collapsed: false, title: 'r' })))
 
-    expect(Zipper.datum(z)).toMatchInlineSnapshot(
-      {
-        title: expect.any(String),
-        id: expect.any(String),
-      },
-      `
-Object {
-  "collapsed": false,
-  "id": Any<String>,
-  "title": Any<String>,
-}
-`,
-    )
+    const lt = Zipper.tree(z)
+    expect(LineTree.title(lt)).toBe('rc1')
+    expect(LineTree.hasVisibleChildren(lt)).toBe(false)
   })
 })
