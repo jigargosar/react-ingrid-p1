46ab54fbb63179f283e9c4165bb7e04be332903f
Jigar Gosar <jigar.gosar@gmail.com>
2019-03-14 14:38:32 +0530

    zipper tests

--- src/LineZipper.test.js
+++ src/LineZipper.test.js
@@ -1,8 +1,11 @@
 import * as Tree from './Tree'
+import * as LineZipper from './LineZipper'
 
 describe('LineZipper', function() {
   it('should do something', function() {
     const tree = Tree.fromDatum('a')
     expect(Tree.datum(tree)).toBe('a')
+    const z = LineZipper.initial
+    expect(z).toBeDefined()
   })
 })
