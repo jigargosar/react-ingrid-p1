a4f644d7f6b237b2b68ab24974ddb08696bda6d1
Jigar Gosar <jigar.gosar@gmail.com>
2019-03-14 13:33:52 +0530

    zipper

--- src/LineZipper.js
+++ src/LineZipper.js
@@ -1,7 +1,7 @@
 import validate from 'aproba'
 import * as Zipper from './TreeZipper'
 import * as LineTree from './LineTree'
-import { either, ifElse, isNil, T } from 'ramda'
+import { either, ifElse, isNil, pipe, T } from 'ramda'
 
 export { Zipper as Z, LineTree as LT }
 
@@ -11,11 +11,16 @@ export function foo() {
   return 1
 }
 
-export function isVisible(z) {
-  const fn = ifElse(
-    either(isNil, LineTree.hasVisibleChildren),
-    T,
-    isVisible,
+function hasVisibleChildren(z) {
+  validate('O', arguments)
+  const fn = pipe(
+    Zipper.tree,
+    LineTree.hasVisibleChildren,
   )
+  return fn(z)
+}
+
+export function isVisible(z) {
+  const fn = ifElse(either(isNil, hasVisibleChildren), T, isVisible)
   return fn(Zipper.parent(z))
 }
