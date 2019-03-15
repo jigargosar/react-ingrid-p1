83abf569a761a506d8ece2eed4060caabc59b4a8
Jigar Gosar <jigar.gosar@gmail.com>
2019-03-15 13:50:41 +0530

    editMode

--- src/EditMode.js
+++ src/EditMode.js
@@ -1,9 +1,10 @@
-import { prop } from 'ramda'
+import { both, prop } from 'ramda'
 
 export const stopEditing = () => ({ editing: false, isNew: null })
 export const startEditing = () => ({ editing: true, isNew: false })
 export const startEditingNew = () => ({ editing: true, isNew: true })
 
 export const isEditing = prop('editing')
+export const isEditingNew = both(prop('editing'), prop('isNew'))
 
 export const initial = { editing: false, isNew: null }
